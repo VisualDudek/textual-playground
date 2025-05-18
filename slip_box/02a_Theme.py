0b4997e335e152e531b7d62bdb8747d144745754
MarcinDudek <dudek.v@gmail.com>
2025-05-18 13:24:42 +0200

    feat: implement initial ContainerApp with Header, Footer, and Label widgets

---
+++ slip_box/02a_Theme.py
@@ -0,0 +1,30 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Label, Footer
+from textual.containers import Horizontal
+
+
+
+class ContainerApp(App):
+
+    def compose(self):
+        yield Header()
+        yield Footer()
+        with Horizontal(id="main_container"):
+            yield Label("Hello, World!")
+            yield Label("Hello, World!")
+
+        # Also works but above is more readable
+        yield Horizontal(
+            Label("Hello, World!"),
+            Label("Hello, World!"),
+            id="horizontal_container"
+        )
+
+    def on_mount(self) -> None:
+        """Called when the app is mounted."""
+        self.theme = "gruvbox"
+
+    
+
+if __name__ == "__main__":
+    ContainerApp().run()
