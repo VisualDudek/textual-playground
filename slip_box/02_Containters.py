51607d08ff46496ef82e9c840a2bf565122a9bbd
MarcinDudek <dudek.v@gmail.com>
2025-05-18 11:58:56 +0200

    feat: add initial implementation of ContainerApp with Header and Label widgets

---
+++ slip_box/02_Containters.py
@@ -0,0 +1,32 @@
+"""
+This script demonstrates:
+- Basic Textual App structure.
+- UI layout using the `compose` method.
+- Usage of `Header` and `Label` widgets.
+- Arranging widgets horizontally using `Horizontal` container.
+- Two methods for adding children to a container.
+"""
+
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Label
+from textual.containers import Horizontal
+
+
+
+class ContainerApp(App):
+
+    def compose(self):
+        yield Header()
+        with Horizontal(id="main_container"):
+            yield Label("Hello, World!")
+            yield Label("Hello, World!")
+
+        # Also works but above is more readable
+        yield Horizontal(
+            Label("Hello, World!"),
+            Label("Hello, World!"),
+            id="horizontal_container"
+        )
+
+if __name__ == "__main__":
+    ContainerApp().run()
